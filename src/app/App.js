7eb2760755263ffb94850ed1e4392bc0398bbd31
johnnyw86 <132710931+johnnyw86@users.noreply.github.com>
2024-07-11 08:33:21 -0400

    Create and rearrange files

---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Search } from '../features/search/Search';
+
+function App() {
+  
+  return (
+    <main>
+      <section> 
+        <Search></Search>
+
+      </section>
+
+
+
+
+    </main>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/features/search/Search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import redditJr from '.../assets/images/redditJr';
+
+
+
+export function Search() {
+    
+    
+    return (
+        <div id="header">
+            <img src={redditJr}/>
+            <p>TEST</p>
+
+
+        </div>
+
+    )
+
+}
