fb333cda371008786941ddc2217cd06d5138ac79
johnnyw86 <132710931+johnnyw86@users.noreply.github.com>
2024-07-19 11:24:34 -0400

    Fix sidebar icons and add links

--- src/features/mainLayout/sideBarNav/SideBarNav.js
+++ src/features/mainLayout/sideBarNav/SideBarNav.js
@@ -13,7 +13,7 @@ export function SidebarNav () {
                 {Object.keys(sidebarIcons).map((key, index) => (
                     <div className={styles.navItems} key={index}>
                         <img className={styles.navIcons} src={sidebarIcons[key].path} alt={sidebarIcons[key].title}/>
-                        <p className={styles.navLinks}>{sidebarIcons[key].title}</p>
+                        <a className={styles.navLinks} href={sidebarIcons[key].url} target='_blank'>{sidebarIcons[key].title}</a>
                     </div>
             ))}
             </div>
--- src/features/mainLayout/sideBarNav/importSidebarIcons.js
+++ src/features/mainLayout/sideBarNav/importSidebarIcons.js
@@ -1,19 +1,24 @@
 function importSidebarIcons(r) {
-    let images = {};
+  let images = {};
+  const baseUrl = 'https://www.reddit.com/r/';
   
-    r.keys().map((item, index) => {
+    r.keys().map((item) => {
+      console.log(item);
       const fileName = item.replace('./', '').replace(/\.[^/.]+$/, ''); // Remove './' and the file extension
+      const fileNameNoSpace = fileName.replace(/\s+/g, "")
       images[fileName] = {
         path: r(item),
-        fullTitle: item,
-        title: fileName.replace(/-/g, ' ') // Replace hyphens with spaces for the title
-      };
-    });
-  
+        title: fileName.replace(/-/g, ' '), // Replace hyphens with spaces for the title
+        url: baseUrl+fileNameNoSpace
+        };
+        });
     return images;
   }
 
 const sidebarIcons = importSidebarIcons(require.context('../../../assets/images/subReddits', false, /\.(png|jpe?g|svg)$/));
+
+console.log(sidebarIcons);
+
 
 export default sidebarIcons; 
 
